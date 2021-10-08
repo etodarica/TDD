698e957b7f0a02d4e912da60f53cf69037516f98
etodarica <etodarica@users.noreply.github.com>
2021-10-08 12:11:53 +0200

    Test und Klasse hinzugefügt

---
+++ TDD_CI_Test/src/GeradeZahl.java
@@ -0,0 +1,17 @@
+
+public class GeradeZahl {
+
+	public GeradeZahl() {
+		super();
+	}
+	
+	public boolean testeZahl(int i) {
+		if(i%2 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
---
+++ TDD_CI_Test/src/TestGeradeZahl.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGeradeZahl {
+	
+	GeradeZahl gz = new GeradeZahl();
+	
+	/* 
+	@Test
+	
+	void test() {
+		fail("Not yet implemented");
+	}
+	*/
+	
+	/* JUnit API documentation:
+	 * https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+	 */
+	@Test
+	void geradeZahl() {
+		boolean ergebnis = gz.testeZahl(4);
+		//assertEquals(Object expected, Object actual)
+		assertEquals(true, ergebnis);
+	}
+	
+	@Test
+	void ungeradeZahl() {
+		boolean ergebnis = gz.testeZahl(3);
+		//assertEquals(Object expected, Object actual)
+		assertEquals(false, ergebnis);
+	}
+
+}
